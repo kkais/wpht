3a119a76dd4784e7510c31b547edd2e563850931
Khurram K <kkais786@gmail.com>
2015-03-14 00:54:17 +0500

    Interval parameter passed to JS and testimonials words truncation function for 140 characters

--- includes/js/infinite-rotator.js
+++ includes/js/infinite-rotator.js
@@ -1,42 +1,42 @@
-jQuery(document).ready(function() { //start after HTML, images have loaded
- 
+jQuery(document).ready(function () { //start after HTML, images have loaded
+
     var InfiniteRotator =
-    {
-        init: function(itemInterval)
-        {
-            //initial fade-in time (in milliseconds)
-            var initialFadeIn = 1000;
- 
-            //interval between items (in milliseconds)
-            var itemInterval = itemInterval;
- 
-            //cross-fade time (in milliseconds)
-            var fadeTime = 2500;
- 
-            //count number of items
-            var numberOfItems = jQuery('.rotating-item').length;
- 
-            //set current item
-            var currentItem = 0;
- 
-            //show first item
-            jQuery('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
- 
-            //loop through the items
-            var infiniteLoop = setInterval(function(){
-                jQuery('.rotating-item').eq(currentItem).fadeOut(fadeTime);
- 
-                if(currentItem == numberOfItems -1){
-                    currentItem = 0;
-                }else{
-                    currentItem++;
+            {
+                init: function (itemInterval)
+                {
+                    //initial fade-in time (in milliseconds)
+                    var initialFadeIn = 1000;
+
+                    //interval between items (in milliseconds)
+                    var itemInterval = itemInterval;
+
+                    //cross-fade time (in milliseconds)
+                    var fadeTime = 2500;
+
+                    //count number of items
+                    var numberOfItems = jQuery('.rotating-item').length;
+
+                    //set current item
+                    var currentItem = 0;
+
+                    //show first item
+                    jQuery('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+
+                    //loop through the items
+                    var infiniteLoop = setInterval(function () {
+                        jQuery('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+
+                        if (currentItem == numberOfItems - 1) {
+                            currentItem = 0;
+                        } else {
+                            currentItem++;
+                        }
+                        jQuery('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+
+                    }, itemInterval);
                 }
-                jQuery('.rotating-item').eq(currentItem).fadeIn(fadeTime);
- 
-            }, itemInterval);
-        }
-    };
- 
-    InfiniteRotator.init(5000);
- 
+            };
+
+    InfiniteRotator.init(interval);
+
 });
